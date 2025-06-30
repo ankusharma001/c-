95123118eb7247b05d95ff99a175d9de03de01ba
Ankush Sharma <sharmankush004a@gmail.com>
2025-06-30 23:35:21 +0530

    newupdates

--- k.cpp
+++ k.cpp
@@ -1,140 +1,94 @@
-
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#include <typeinfo>
 #include <bits/stdc++.h>
+//#include <iostream>
+ 
 using namespace std;
+ 
+//Create a simple ECommerce model: User places an Order that contains multiple Items. Write classes for each, and show how a user can place an order and view order summary.
 
-int fact (int  n )
+class Item
 {
-  if (n==0)
-  {
-    return 1;
-  }
-  int first = fact(n-1);
-  int second = n*first;
+private:
+    string name;
+    double price;
+    int quantity;
 
-  return second;
+public:
+Item(string name, double price,int quantity)
+{
+    this -> name = name;
+    this -> price = price;
+     this -> quantity = quantity;
 }
+double getprice()const
+{
+    return price * quantity;
+}
+
+void display() const {
+        cout << name << " - Rs. " << price 
+             << " x " << quantity 
+             << " = Rs. " << getprice() << endl;
+    }
+   
+};
+
+
+class Order
+{
+private:
+   vector <Item> Items;
+public:
+void additem(const Item& item){
+    Items.push_back(item);
+}
+ void displaySummary() const {
+        if (Items.empty()) {
+            cout << "Your order is empty." << endl;
+            return;
+        }
+
+        cout << "Order Summary:\n";
+        double total = 0;
+        for (const Item& item : Items) {
+            item.display();
+            total += item.getprice();
+        }
+        cout << "Total Amount: Rs. " << total << endl;
+    }
+   
+};
+
+class User{
+    private:
+    string username;
+    Order order;
+    public:
+
+    User(string name) : username(name) {}
+
+    void placeItem(const Item& item) {
+        order.additem(item);
+    }
+
+    void viewOrder() const {
+        cout << "\nOrder for " << username << ":\n";
+        order.displaySummary();
+    }
+
+};
+
+
 
 int main()
 {
 
-//   vector <int > arr = {1,2,1,5,-1,0,-2,-8,0};
-//   vector <int> zero ;
-//    vector <int> pos;
-//    vector<int> neg;
+    User user("Ankush");
 
-//    sort (arr.begin(), arr.end());
-//    for(int i =0; i< arr.size();i++ )
-//    {
-//     if (arr[i] == 0)
-//     {
-//         zero.push_back(arr[i]);
-//     }
-//     else if (arr[i]>0)
-//     {
-//         pos.push_back(arr[i]);
-//     }else{
-//         neg.push_back(arr[i]);
-//     }
-//    }
+    user.placeItem(Item("Laptop", 50000, 1));
+    user.placeItem(Item("Mouse", 800, 2));
+    user.placeItem(Item("Keyboard", 1200, 1));
 
-//    sort(neg.rbegin(), neg.rend());
+    user.viewOrder();
 
-//    for(int i =0 ;i<zero.size();i++)
-//    {
-//     cout <<zero[i]<< "";
-//    }
-//    cout<<endl;
-//   for(int i =0 ;i<pos.size();i++)
-//    {
-//     cout <<pos[i];
-//    }
-//    cout<<endl;
-//   for(int i =0 ;i<neg.size();i++)
-//    {
-//     cout <<neg[i];
-//    }
-//    cout<<endl;
-
-//    vector <int> ans;
-//    int size =0;
-//    int ns = neg.size();
-//    int ps= pos.size();
-   
-
-//    if (ns>ps)
-//    {
-//     size= ps;
-//    }
-//    else{
-//     size= ns;
-//    }
-//    for (int i = 0; i < size; i++)
-//    {
-//     if ( abs(neg[i]) > pos[i] )
-//     {
-//         ans.push_back(pos[i]);
-//         ans.push_back(neg[i]);
-//     }
-//     else if (abs(neg[i])== pos[i])
-//     {
-//         ans.push_back(neg[i]);
-//         ans.push_back(pos[i]);
-//     }
-//    }
-//    if (ns!=ps)
-//    {
-//     if(ps > ns)
-//     {
-//         for(int i = ns ; i < ps;i++)
-//         {
-//             ans.push_back(pos[i]);
-//         }
-//     }
-//     else{
-//         for(int i =ps ; i< ns;i++)
-//         {
-//             ans.push_back(neg[i]);
-//         }
-//     }
-
-//    }
-//    cout<<endl;
-//    for(int i =0;i< ans.size();i++)
-//    {
-//     cout<< ans[i]<< " ";
-//    }
-
-
-    vector<vector<int>> arr = {
-        {1, 2, 4},
-        {10, 8, 9},
-        {9, 8, 5}
-    };
-
-    for(auto & row : arr)
-    {
-    sort(row.begin(), row.end());
-    }
-    for (auto& row : arr) {
-        for (auto& num : row) {
-            cout << num << " ";
-        }
-        cout << endl;
-    }
-
-    string str = "abc";
-
-    cout << "\"\"" << endl; 
-
-    for (int i = 0; i < str.length(); i++) {
-        for (int j = i; j < str.length(); j++) {
-            cout << str.substr(i, j - i + 1) << endl;
-        }
-    }  
-
-    return 0;
+  return 0;
 }
